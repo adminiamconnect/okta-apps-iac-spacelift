46afae2650ea09f91ed654fafe29f1325699087a
Michael Corrodus <michael.corrodus@ft.com>
2025-11-12 21:36:55 +0000

    changes

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,9 +1,11 @@
 terraform {
   required_version = ">= 1.5.0"
+
   required_providers {
     okta = {
       source  = "okta/okta"
-      version = "6.1.0"
+      version = "= 6.1.0"
     }
+    // ...existing provider entries...
   }
 }
