8a96172384e2de2d59808d6c80145f8e97c19993
michaelcorrodus <admin@iamconnect.co.uk>
2025-11-12 16:11:55 +0000

    Delete okta-apps-iac-spacelift directory

--- okta-apps-iac-spacelift/infra/main.tf
+++
@@ -1,9 +0,0 @@
-# Example root composition (optional if using env/* entrypoints)
-
-module "web_oidc_example" {
-  source        = "./modules/okta-app-web-oidc"
-  label         = "example-web-oidc"
-  redirect_uris = ["http://localhost:3000/callback"]
-  post_logout_redirect_uris = ["http://localhost:3000"]
-  group_names   = ["EXAMPLE_WEB_OIDC_DEVS"]
-}
--- okta-apps-iac-spacelift/infra/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    okta = {
-      source  = "okta/okta"
-      version = "= 6.1.0"
-    }
-  }
-}
