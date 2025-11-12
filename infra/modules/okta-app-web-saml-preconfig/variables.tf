142f2e58376d83dfa947016c2e350da9cccd6a5c
michaelcorrodus <admin@iamconnect.co.uk>
2025-11-12 17:22:04 +0000

    Update variables.tf

--- infra/modules/okta-app-web-saml-preconfig/variables.tf
+++ infra/modules/okta-app-web-saml-preconfig/variables.tf
@@ -1,4 +1,17 @@
-variable "label" { type = string }
-variable "preconfigured_app" { type = string } # e.g., "boxnet", "zendesk"
-variable "group_names" { type = list(string) default = [] }
-variable "group_ids" { type = list(string) default = [] }
+variable "label" {
+  type = string
+}
+
+variable "preconfigured_app" {
+  type = string # e.g., "boxnet", "zendesk"
+}
+
+variable "group_names" {
+  type    = list(string)
+  default = []
+}
+
+variable "group_ids" {
+  type    = list(string)
+  default = []
+}
