aeea0acfa06135e0020c8f5417fc853340bdb217
michaelcorrodus <admin@iamconnect.co.uk>
2026-02-25 13:13:01 +0000

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,13 @@
+#######################################################
+# Example: Okta Applications and Groups (single env)
+#######################################################
+
+# Shared groups
+module "shared_groups" {
+  source      = "./modules/okta-groups"
+  group_names = ["IAMCONNECT_SHARED_USERS"]
+}
+
 ######## WEB OIDC ########
 module "IAM_Connect" {
   source                    = "./modules/okta-app-web-oidc"
